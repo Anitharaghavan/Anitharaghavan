4be385df3e05d20370a89e1b32c3afc6941ca338
Anitharaghavan <anitharaghavan005@gmail.com>
2016-06-06 21:16:51 +0530

    Create numberpalindrome.java

---
+++ numberpalindrome.java
@@ -0,0 +1,24 @@
+
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		String str=sc.next();
+		String rev=new StringBuffer(str).reverse().toString();
+		if(str.equals(rev))
+		{
+			System.out.println("palindrome");
+		}
+		else
+		{
+			System.out.println("Not palindrome");
+		}
+	}
+}
