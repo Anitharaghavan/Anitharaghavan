23414bb0c00d19a46558996849c90121ff275f99
Anitharaghavan <anitharaghavan005@gmail.com>
2016-06-06 22:11:30 +0530

    Create reversedigi-p.java

---
+++ reversedigi-p.java
@@ -0,0 +1,17 @@
+
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		String str=sc.next();
+		String rev=new StringBuffer(str).reverse().toString();
+		System.out.println(rev);
+	}
+}
